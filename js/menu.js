20ef5bf63621da0a634a93b858b3294fa2f6f5f3
karlozpo <carlosposadat@gmail.com>
2020-02-26 18:25:08 -0500

    ajustes cuadros 26 feb

--- js/menu.js
+++ js/menu.js
@@ -16,43 +16,43 @@ for (i = 0; i < x.length; i++) {
     create a new DIV that will act as an option item: */
     c = document.createElement("DIV");
     c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
-        /* When an item is clicked, update the original select box,
-        and the selected item: */
-        var y, i, k, s, h;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        h = this.parentNode.previousSibling;
-        for (i = 0; i < s.length; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            
-           /* Con este swich dependiendo de la posición del elemento pongo el estilo o lo retiro */
-            switch (s.selectedIndex) {
-                case 1:
-                    document.body.classList.remove("dark");
-                    
-                    break;
-                case 2:
-                    document.body.classList.add("dark");
-                    
-                    break;
-            }
+    c.addEventListener("click", function (e) {
+      /* When an item is clicked, update the original select box,
+      and the selected item: */
+      var y, i, k, s, h;
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      h = this.parentNode.previousSibling;
+      for (i = 0; i < s.length; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
 
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            for (k = 0; k < y.length; k++) {
-              y[k].removeAttribute("class");
-            }
-            this.setAttribute("class", "same-as-selected");
-            break;
+          /* Con este swich dependiendo de la posición del elemento pongo el estilo o lo retiro */
+          switch (s.selectedIndex) {
+            case 1:
+              document.body.classList.remove("dark");
+
+              break;
+            case 2:
+              document.body.classList.add("dark");
+
+              break;
           }
+
+          h.innerHTML = this.innerHTML;
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+          for (k = 0; k < y.length; k++) {
+            y[k].removeAttribute("class");
+          }
+          this.setAttribute("class", "same-as-selected");
+          break;
         }
-        h.click();
+      }
+      h.click();
     });
     b.appendChild(c);
   }
   x[i].appendChild(b);
-  a.addEventListener("click", function(e) {
+  a.addEventListener("click", function (e) {
     /* When the select box is clicked, close any other select boxes,
     and open/close the current select box: */
     e.stopPropagation();
@@ -75,13 +75,13 @@ function closeAllSelect(elmnt) {
     } else {
 
       y[i].classList.remove("select-arrow-active");
-      
+
     }
   }
   for (i = 0; i < x.length; i++) {
     if (arrNo.indexOf(i)) {
       x[i].classList.add("select-hide");
-     
+
     }
   }
 }
@@ -97,14 +97,14 @@ then close all select boxes: */
 
 function wow(value) {
   switch (value) {
-      case "light":
-          document.body.classList.remove("dark");
-          alert("light");
-          break;
-      case "dark":
-          document.body.classList.add("dark");
-          alert("dark");
-          break;
+    case "light":
+      document.body.classList.remove("dark");
+      alert("light");
+      break;
+    case "dark":
+      document.body.classList.add("dark");
+      alert("dark");
+      break;
   }
 }
 
@@ -112,42 +112,47 @@ function wow(value) {
 
 const url = 'https://api.giphy.com/v1/gifs/search?';
 const apiKey = 'api_key=HfFHI1IWTIBPUa7JgXcE0M67VgBCex81&q=';
-let searchResult='default';
-let numbersofGifs="10";
+let searchResult = 'trending';
+let numbersofGifs = "4";
+
+
 
 
 function search() {
-    document.getElementById("gifsContainer").innerHTML = "";
-searchResult =document.getElementById("buscador").value;  
+  document.getElementById("gifsContainer").innerHTML = "";
+    searchResult = document.getElementById("buscador").value;
 
-const getUrls = async () => {
-const response = await fetch(url+apiKey+searchResult+"&limit="+numbersofGifs+"&offset=0&rating=G&lang=es", { method: 'get' });
-const json = await response.json();
-return json.data.map(data => data.images.original.url);
+  const getUrls = async () => {
+    const response = await fetch(url + apiKey + searchResult + "&limit=" + numbersofGifs + "&offset=0&rating=G&lang=es", {
+      method: 'get'
+    });
+    const json = await response.json();
+    return json.data.map(data => data.images.fixed_width.url);
+  }
+
+  getUrls().then(urls => {
+
+    urls.forEach((url) => {
+      let divContenedor = document.createElement("div");
+      let imgContenedor = document.createElement("img");
+      divContenedor.classList.add("cuadroGif");
+      imgContenedor.setAttribute("src", url);
+      imgContenedor.setAttribute("width", "auto");
+      console.log(url);
+      divContenedor.appendChild(imgContenedor);
+      document.getElementById("gifsContainer").appendChild(divContenedor);
+
+    });
+  });
+
+
 }
 
-getUrls().then(urls => {
-    
-urls.forEach((url) => {
-   let divContenedor=document.createElement("div");
-   let imgContenedor=document.createElement("img");
-   divContenedor.classList.add("cuadroGif");
-   imgContenedor.setAttribute("src", url);
-   imgContenedor.setAttribute("width", "auto");
-   console.log(url);
-   divContenedor.appendChild(imgContenedor);
-document.getElementById("gifsContainer").appendChild(divContenedor);
-
-});
-});
-
-
-}
-
+search();
 
 /// key press buscador
-document.getElementById('buscador').onkeypress = function(e) {
-  if(e.keyCode == 13) {
-      search();
+document.getElementById('buscador').onkeypress = function (e) {
+  if (e.keyCode == 13) {
+    search();
   }
 }
