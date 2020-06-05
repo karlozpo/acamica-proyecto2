d10ebad92210d89f061c275f574c1fb375f0b2e8
Carlos Posada <carlosposadat@gmail.com>
2020-06-04 21:44:36 -0500

    Terminado_junio6
    Termine de ajustar el local storage y el session storage mas la creacion de gifs

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,24 @@
+///DROPDOWN  SUPERIOR
 var x, i, j, selElmnt, a, b, c;
-/* Look for any elements with the class "custom-select": */
+/* Busque cualquier elemento con la clase "custom-select": */
 x = document.getElementsByClassName("custom-select");
 for (i = 0; i < x.length; i++) {
   selElmnt = x[i].getElementsByTagName("select")[0];
-  /* For each element, create a new DIV that will act as the selected item: */
+  /* Para cada elemento, cree un nuevo DIV que actuará como el elemento seleccionado: */
   a = document.createElement("DIV");
   a.setAttribute("class", "select-selected");
   a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
   x[i].appendChild(a);
-  /* For each element, create a new DIV that will contain the option list: */
   b = document.createElement("DIV");
   b.setAttribute("class", "select-items select-hide");
   for (j = 1; j < selElmnt.length; j++) {
-    /* For each option in the original select element,
-    create a new DIV that will act as an option item: */
+    /* Para cada opción en el elemento de selección original,
+    cree un nuevo DIV que actuará como un elemento de opción: */
     c = document.createElement("DIV");
     c.innerHTML = selElmnt.options[j].innerHTML;
     c.addEventListener("click", function (e) {
-      /* When an item is clicked, update the original select box,
-      and the selected item: */
+      /* Cuando se hace clic en un elemento, actualice el cuadro de selección original,
+      y el artículo seleccionado: */
       var y, i, k, s, h;
       s = this.parentNode.parentNode.getElementsByTagName("select")[0];
       h = this.parentNode.previousSibling;
@@ -53,8 +53,8 @@ for (i = 0; i < x.length; i++) {
   }
   x[i].appendChild(b);
   a.addEventListener("click", function (e) {
-    /* When the select box is clicked, close any other select boxes,
-    and open/close the current select box: */
+    /* Cuando se hace clic en el cuadro de selección, cierre cualquier otro cuadro de selección,
+    y abra / cierre el cuadro de selección actual: */
     e.stopPropagation();
     closeAllSelect(this);
     this.nextSibling.classList.toggle("select-hide");
@@ -63,35 +63,30 @@ for (i = 0; i < x.length; i++) {
 }
 
 function closeAllSelect(elmnt) {
-  /* A function that will close all select boxes in the document,
-  except the current select box: */
-  var x, y, i, arrNo = [];
+  /* Una función que cerrará todos los cuadros de selección en el documento,
+  excepto el cuadro de selección actual: */
+  var x,
+    y,
+    i,
+    arrNo = [];
   x = document.getElementsByClassName("select-items");
   y = document.getElementsByClassName("select-selected");
   for (i = 0; i < y.length; i++) {
     if (elmnt == y[i]) {
       arrNo.push(i);
-
     } else {
-
       y[i].classList.remove("select-arrow-active");
-
     }
   }
   for (i = 0; i < x.length; i++) {
     if (arrNo.indexOf(i)) {
       x[i].classList.add("select-hide");
-
     }
   }
 }
 
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-
-
-
-
+/* Si el usuario hace clic en cualquier lugar fuera del cuadro de selección,
+luego cierre todos los cuadros de selección: */
 
 ///CAMBIAR COLOR DEL TEMA
 
@@ -110,77 +105,103 @@ function wow(value) {
 
 //BUSCAR GIFS
 
-const url = 'https://api.giphy.com/v1/gifs/search?';
-const apiKey = 'api_key=HfFHI1IWTIBPUa7JgXcE0M67VgBCex81&q=';
-let searchResult = 'trending';
+const url = "https://api.giphy.com/v1/gifs/search?";
+const apiKey = "api_key=HfFHI1IWTIBPUa7JgXcE0M67VgBCex81&q=";
+const apiKeySlash = "?api_key=HfFHI1IWTIBPUa7JgXcE0M67VgBCex81";
+let searchResult = "trending";
 let numbersofGifs = "8";
 
 // Sugerencias de busqueda
-let buscadorTrendings= document.getElementById("buscador");
-let titulosSugeridos= document.getElementById("resultados");
-buscadorTrendings.addEventListener("focus",function () {
-document.getElementById("sugerencias").setAttribute("style","margin-top:120px");
-titulosSugeridos.style.display="block";
-})
-buscadorTrendings.addEventListener("focusout",function () {
-  document.getElementById("sugerencias").setAttribute("style","margin-top:15px");
-  titulosSugeridos.style.display="none";
-  })
+let buscadorTrendings = document.getElementById("buscador");
+let titulosSugeridos = document.getElementById("resultados");
+buscadorTrendings.addEventListener("focus", function () {
+  document
+    .getElementById("sugerencias")
+    .setAttribute("style", "margin-top:180px");
+  titulosSugeridos.style.display = "block";
+});
 
+buscadorTrendings.addEventListener("change", function () {
+  document
+    .getElementById("sugerencias")
+    .setAttribute("style", "margin-top:55px");
+  titulosSugeridos.style.display = "none";
+});
 
 //crear hashtag
-
-
+let contenedorBusquedas = document.getElementById("b_recientes_lista");
+let numeroBusqueda = 0;
 function search() {
   document.getElementById("gifsTendencias").innerHTML = "";
+  searchResult = document.getElementById("buscador").value;
+  if (searchResult !== "") {
+    numeroBusqueda++;
+    sessionStorage.setItem("busquedaNo" + numeroBusqueda, searchResult);
+  }
+  contenedorBusquedas.innerHTML = "";
+  for (let i = 1; i <= numeroBusqueda; i++) {
+    let busqueda = sessionStorage.getItem("busquedaNo" + i);
+    let boton = document.createElement("li");
+    boton.innerHTML = busqueda;
+    boton.className = "botBusqueda";
+    contenedorBusquedas.appendChild(boton);
+    boton.onclick = () => {
+      buscador.value = boton.innerText;
+      search();
+    };
 
-  
-   searchResult = document.getElementById("buscador").value;
-   let textTendencias= document.getElementById("tendencias");
-   textTendencias.innerHTML=searchResult;
-  
-
-  const getUrls = async () => {
-    const response = await fetch(url + apiKey + searchResult + "&limit=" + numbersofGifs + "&offset=0&rating=G&lang=es", {
-      method: 'get'
-    });
-    const json = await response.json();
-    
-    return json.data.map(data => data.images.fixed_width.url);
+    document
+      .getElementById("gifsAutomaticos")
+      .setAttribute("style", "display:block");
+    document.getElementById("misGifs").setAttribute("style", "margin-top:20px");
   }
 
-  getUrls().then(urls => {
+  let textTendencias = document.getElementById("tendencias");
+  textTendencias.innerHTML = searchResult;
+
+  const getUrls = async () => {
+    const response = await fetch(
+      url +
+        apiKey +
+        searchResult +
+        "&limit=" +
+        numbersofGifs +
+        "&offset=0&rating=G&lang=es",
+      {
+        method: "get",
+      }
+    );
+    const json = await response.json();
+
+    return json.data.map((data) => data.images.fixed_width.url);
+  };
+
+  getUrls().then((urls) => {
     var elmnt = document.getElementById("hola");
     elmnt.scrollIntoView();
     console.log(elmnt);
     urls.forEach((url) => {
-      
       let divContenedor = document.createElement("div");
       let imgContenedor = document.createElement("img");
       divContenedor.classList.add("cuadroGif");
       imgContenedor.setAttribute("src", url);
       imgContenedor.setAttribute("width", "auto");
-      
+
       divContenedor.appendChild(imgContenedor);
       document.getElementById("gifsTendencias").appendChild(divContenedor);
-
     });
   });
-
-
 }
 
-
-
-
 /// key press buscador
-document.getElementById('buscador').onkeypress = function (e) {
+document.getElementById("buscador").onkeypress = function (e) {
   if (e.keyCode == 13) {
     search();
   }
-}
+};
 
-const url2= "https://api.giphy.com/v1/gifs/trending?api_key=HfFHI1IWTIBPUa7JgXcE0M67VgBCex81&limit=";
+const url2 =
+  "https://api.giphy.com/v1/gifs/trending?api_key=HfFHI1IWTIBPUa7JgXcE0M67VgBCex81&limit=";
 let numbersofGifs2 = "8";
 
 //LLAMAR TRENDINGS
@@ -189,71 +210,129 @@ function trending() {
 
   const getUrls2 = async () => {
     const response2 = await fetch(url2 + numbersofGifs2 + "&rating=G", {
-      method: 'get'
+      method: "get",
     });
 
     const json2 = await response2.json();
-    return json2.data.map(data => data.images.fixed_width.url);
-  }
+    return json2.data.map((data) => data.images.fixed_width.url);
+  };
 
-  getUrls2().then(urls => {
-
+  getUrls2().then((urls) => {
     urls.forEach((url2) => {
       let divContenedor = document.createElement("div");
       let imgContenedor = document.createElement("img");
       divContenedor.classList.add("cuadroGif");
       imgContenedor.setAttribute("src", url2);
       imgContenedor.setAttribute("width", "auto");
-      
       divContenedor.appendChild(imgContenedor);
       document.getElementById("gifsTendencias").appendChild(divContenedor);
-
     });
   });
-// LLAMAR TOPICOS PRINCIPALES CON HASHTAGS
+  // LLAMAR TOPICOS PRINCIPALES CON HASHTAGS
   const getUrls3 = async () => {
-    const response3 = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=HfFHI1IWTIBPUa7JgXcE0M67VgBCex81&offset=8&limit=4&rating=G", {
-      method: 'get'
-    });
-    
+    const response3 = await fetch(
+      "https://api.giphy.com/v1/gifs/trending?api_key=HfFHI1IWTIBPUa7JgXcE0M67VgBCex81&offset=8&limit=4&rating=G",
+      {
+        method: "get",
+      }
+    );
+
     const json3 = await response3.json();
-   //el parametro index me dice el nimero de interaciones que realiza el MAP
-    return json3.data.map(function (data,index) {
-      
-      let x=document.getElementsByClassName("imgtrend");
-      x[index].setAttribute("src",data.images.fixed_width.url);
-      let y=document.getElementsByClassName("tituloTrending");
+    //el parametro index me dice el nimero de interaciones que realiza el MAP
+    return json3.data.map(function (data, index) {
+      let x = document.getElementsByClassName("imgtrend");
+      x[index].setAttribute("src", data.images.fixed_width.url);
+      let y = document.getElementsByClassName("tituloTrending");
       //crear hashtag
-      let titular=data.title;
-      let hashtag="#"+titular.replace(/ .*/,'');
-       //crear Url del ver más
-      let mas=document.getElementsByClassName("masLink");
-      mas[index].setAttribute("href","https://giphy.com/search/"+titular.replace(/ .*/,''));
-      y[index].innerHTML=hashtag;
-      
-      let topic=document.getElementsByClassName("tendencia");
-      topic[index].innerHTML=titular.replace(/ .*/,'');
-      
-    
-     
-      
+      let titular = data.title;
+      let hashtag = "#" + titular.replace(/ .*/, "");
+      //crear Url del ver más
+      let mas = document.getElementsByClassName("masLink");
+      mas[index].setAttribute(
+        "href",
+        "https://giphy.com/search/" + titular.replace(/ .*/, "")
+      );
+      y[index].innerHTML = hashtag;
+      let topic = document.getElementsByClassName("tendencia");
+      topic[index].innerHTML = titular.replace(/ .*/, "");
       return null;
-    })
-    
-  }
-  getUrls3().then(function () {
-
-  });
-
+    });
+  };
+  getUrls3().then(function () {});
 }
 
 trending();
 
 // Tendencias Recomendadas al click
-let tendencia1= document.getElementById("tendencia1");
-let tendencia2= document.getElementById("tendencia2");
-let tendencia3= document.getElementById("tendencia3");
+let tendencia1 = document.getElementById("tendencia1");
+let tendencia2 = document.getElementById("tendencia2");
+let tendencia3 = document.getElementById("tendencia3");
 
-tendencia1.addEventListener("click",function () {
-  console.log("hello");
-})
+tendencia1.addEventListener("click", function () {
+  buscador.value = tendencia1.textContent;
+  titulosSugeridos.style.display = "none";
+  document
+    .getElementById("sugerencias")
+    .setAttribute("style", "margin-top:55px");
+  search();
+});
+
+tendencia2.addEventListener("click", function () {
+  buscador.value = tendencia2.textContent;
+  titulosSugeridos.style.display = "none";
+  document
+    .getElementById("sugerencias")
+    .setAttribute("style", "margin-top:55px");
+  search();
+});
+
+tendencia3.addEventListener("click", function () {
+  buscador.value = tendencia3.textContent;
+  titulosSugeridos.style.display = "none";
+  document
+    .getElementById("sugerencias")
+    .setAttribute("style", "margin-top:55px");
+  search();
+});
+
+let misgifos = document.getElementById("migifos");
+misgifos.addEventListener("click", function () {
+  document
+    .getElementById("gifsAutomaticos")
+    .setAttribute("style", "display:none");
+  document.getElementById("misGifs").setAttribute("style", "margin-top:70px");
+});
+
+function guifos() {
+  if (localStorage.length != 0) {
+    for (let i = 0; i <= localStorage.length; i++) {
+      let clave = localStorage.key(i);
+      let idGif = localStorage.getItem(clave).slice(1, -1);
+      let creadoGif = fetch(
+        "https://api.giphy.com/v1/gifs/" + idGif + apiKeySlash
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let objeto = data.data.images.fixed_height.url;
+          let divContenedor = document.createElement("div");
+          let imagen = document.createElement("img");
+          imagen.src = objeto;
+
+          let divGuifos = document.querySelector("#imagenesGuifos");
+
+          divGuifos.appendChild(imagen);
+
+          divGuifos.classList.add("cuadroGif3");
+
+          imagen.setAttribute("width", "auto");
+        })
+        .catch((error) => {
+          return error;
+        });
+    }
+  }
+}
+
+guifos();
--- js/upload.js
+++ js/upload.js
@@ -21,9 +21,24 @@ const video = document.querySelector("video");
 const record = document.getElementById("record");
 const stop = document.getElementById("stop");
 
+const subir = document.getElementById("subir");
+const repetir = document.getElementById("repetir");
+const listo = document.getElementById("listo");
+const copiarLink = document.getElementById("copiarLink");
+
+const botonIniciar2 = document.getElementById("record");
+const botonDetener2 = document.getElementById("stop");
+
+record.setAttribute("style","display:none");
+botonDetener2.setAttribute("style","display:none");
+subir.setAttribute("style","display:none");
+repetir.setAttribute("style","display:none");
+const form = new FormData();
+
+
 // Fefinimos el objeto recorder - tiene que se global para que podamos accederlo en todos los listeners
 let recorder;
-
+var detenerCamara=null;
 function getStreamAndRecord() {
   navigator.mediaDevices
     .getUserMedia({
@@ -36,6 +51,7 @@ function getStreamAndRecord() {
 
     .then(function(stream) {
       video.srcObject = stream;
+      detenerCamara=stream;
       video.play();
       recorder = RecordRTC(stream, {
         type: "gif",
@@ -48,8 +64,10 @@ function getStreamAndRecord() {
           console.log("started");
         }
       });
+      
     });
 }
+var varidGif;
 
 function uploadGif(gif) {
   // formateamos el post según las necesidades particulares de la api de giphy
@@ -68,8 +86,9 @@ function uploadGif(gif) {
     .then(data => {
       console.log(data);
       // guardar en localStorage
+      varidGif = data.data.id ;
       saveGifLoacalStorage(data.data.id)
-
+     
     })
     .catch(error => {
       uploadMessage.innerHTML = `<h3>Hubo un error subiendo tu Guifo</h3>`;
@@ -78,37 +97,146 @@ function uploadGif(gif) {
 }
 
 function saveGifLoacalStorage(id){
+  
     localStorage.setItem(`${id}`, JSON.stringify(id))
 }
+document.getElementById("contadorFull").setAttribute("style","display:none");
+document.getElementById("subiendoGif").setAttribute("style","display:none");
+const gifFinal= document.getElementById("gifFinal");
+gifFinal.setAttribute("style","display:none");
 
 // Evento click sobre el boton para iniciar stream
 start.addEventListener("click", function() {
   console.log("llamar a la funcion stream");
   getStreamAndRecord();
+  botonIniciar2.setAttribute("style","display:none");
+  start.setAttribute("style","display:none");
+  record.setAttribute("style","display:block");
+  document.getElementById("tituloT").textContent = "Capturando tu Guifo";
+  document.getElementById("contadorFull").setAttribute("style","display:block");
 });
 
 // Evento click para iniciar grabacion
 record.addEventListener("click", function() {
   console.log("llamar a la funcion startRecording");
   recorder.startRecording();
+  record.setAttribute("style","display:none");
+  botonDetener2.setAttribute("style","display:block");
+  document.getElementById("contadorFull").setAttribute("style","display:block");
+  contador();
 });
-
+var gifcaptura;
 // Evento click para parar grabacion
 stop.addEventListener("click", function() {
+  document.getElementById("tituloT").textContent = "Vista Previa";
+
+  botonDetener2.setAttribute("style","display:none");
+  subir.setAttribute("style","display:block");
+  repetir.setAttribute("style","display:block");
+  
   recorder.stopRecording(function() {
-    let form = new FormData();
     // creamos nuestro archivo gif
     form.append("file", recorder.getBlob(), "carlos.gif");
-    let gifcaptura= document.getElementById("gifCaptura");
+    gifcaptura= document.getElementById("gifCaptura");
     gifcaptura.src = URL.createObjectURL(recorder.getBlob());
     document.getElementById("videoCaptura").setAttribute("style","display:none");
-    recorder.camera.stop();
-    recorder.destroy();
-    recorder = null;
-    
     console.log(form.get("file"));
     console.log(recorder.getBlob());
-    // subimos nuestro gif
-    uploadGif(form);
+    detenerCamara.getTracks().forEach(track => track.stop());
+    document.getElementById("contadorFull").setAttribute("style","display:none");
+  
   });
 });
+
+subir.addEventListener("click", function(){
+  console.log("subiendo");
+   // subimos nuestro gif
+  uploadGif(form);
+  //detener camara
+  subir.setAttribute("style","display:none");
+  repetir.setAttribute("style","display:none");
+  gifcaptura.setAttribute("style","display:none");
+  document.getElementById("subiendoGif").setAttribute("style","display:flex");
+  mostrarGifFinal();
+});
+
+
+let mostrarGifFinal= function () {
+  setTimeout(function(){ 
+    document.getElementById("tituloT").textContent = "Guifo subido con exito";
+    document.getElementById("subiendoGif").setAttribute("style","display:none");
+    gifFinal.setAttribute("style","display:block");
+    gifcaptura.classList.add("contentFinal")
+    gifcaptura.setAttribute("style","display:block");
+    gifFinal.prepend(gifcaptura);
+  }, 2000);
+}
+
+repetir.addEventListener("click", function(){
+  location.reload();
+});
+
+listo.addEventListener("click", function(){
+  window.open("index.html",'_self');
+});
+
+copiarLink.addEventListener("click", function(){
+  let urlGif = "https://media.giphy.com/media/"+ varidGif +"/giphy.gif";
+  window.open(urlGif,'_blank');
+});
+
+let paso1=document.getElementById("paso1");
+paso1.addEventListener("click",function () {
+  let ventana1= document.getElementById("ventanaPaso1");
+  let ventana2= document.getElementById("gifCreador1");
+  ventana1.setAttribute("style","display:none");
+  ventana2.setAttribute("style","display:block");
+
+  
+})
+
+const contador = function () {
+  var seconds = document.getElementById("contador").textContent;
+  const countdown = setInterval(function() {
+    seconds++;
+    if(seconds<10){
+      let numero="0";
+      let secunderos= seconds.toString();
+      let numeroSuma= numero+secunderos;
+      document.getElementById("contador").textContent = numeroSuma;
+    }
+    else{
+      document.getElementById("contador").textContent = seconds;
+    }
+    
+    
+    if (seconds >= 60) clearInterval(countdown);
+}, 1000);
+}
+
+
+
+function guifos(){
+  document.querySelector(".misGifs").style.display= "block";
+  if (localStorage.length != 0) {
+  for(let i=0; i<=localStorage.length; i++){
+      let clave = localStorage.key(i);
+      let idGif = localStorage.getItem(clave).slice(1, -1);
+      let creadoGif= fetch('https://api.giphy.com/v1/gifs/'+idGif+'?api_key='+ apiKey)
+      .then(response => {
+      return response.json(); })
+      .then(data =>{
+              let objeto= data.data.images.fixed_height.url;;
+              let imagen = document.createElement("img");
+              imagen.src = objeto;
+              imagen.className= "imgGuifo";
+              let divGuifos = document.querySelector("#imagenesGuifos");
+              divGuifos.appendChild(imagen);
+           })
+           .catch(error => { return error; 
+          }); 
+          
+    }    
+}}
+
+guifos();
